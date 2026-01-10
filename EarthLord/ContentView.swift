692c070e42ef2ecd31392bad003a2246daa02ca5
Yiguo27 <yiguo2027@outlook.com>
2026-01-10 11:04:48 +0800

    Initial Commit

---
+++ EarthLord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EarthLord
+//
+//  Created by 赵燕燕 on 2026/1/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by 赵燕燕 on 2026/1/10.
+//
+
+import SwiftUI
+
+@main
+struct EarthLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
